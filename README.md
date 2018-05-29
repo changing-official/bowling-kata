405c28fe6d3569f7d37c8c3dd0831e4dc158c895
Alison Henrique Jonck <alisonjonck@gmail.com>
2018-05-28 22:20:44 -0300

    travis

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bowling-kata
 Bowling Game Kata
+
+[![Build Status](https://travis-ci.org/changing-official/bowling-kata.svg?branch=dev)](https://travis-ci.org/changing-official/bowling-kata)
---
+++ bowling-test.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestBowlingKata(unittest.TestCase):
+
+    def testBowlingGameInstance(self):
+        self.assertFail()
+
+if __name__ == '__main__':
+    unittest.main()
